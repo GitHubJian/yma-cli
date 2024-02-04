6174cab61bb9f434d0a4b146c50261789f1d1b3a
xiaowensheng <jian958753831@qq.com>
2024-02-04 16:12:06 +0800

    feat: import

---
+++ package/babel-require-context-loader/README.md
@@ -0,0 +1,8 @@
+# yma-babel-require-context-loader
+
+# Usage
+
+```js
+__require_context__(); // 与 webpack 的 require.context 参数含义相同，替换为 require() 类型
+__require_context__.import(); // 替换为 import("") 类型
+```
--- package/babel-require-context-loader/src/transform.js
+++ package/babel-require-context-loader/src/transform.js
@@ -7,6 +7,61 @@ const generate = require('@babel/generator').default;
 const t = require('@babel/types');
 
 const FUNCTION_NAME = '__require_context__';
+const PROPERTY_IMPORT = 'import';
+
+function createRequireStatement(paths) {
+    return paths.map(function (p) {
+        return t.expressionStatement(
+            t.callExpression(t.identifier('require'), [t.stringLiteral(p)])
+        );
+    });
+}
+
+function createImportStatement(paths) {
+    return paths.map(function (p) {
+        return t.importDeclaration([], t.stringLiteral(p));
+    });
+}
+
+function getFilepaths(filename, nodeArguments) {
+    const args = nodeArguments;
+
+    let directory, includeSubdirectories, regularExpression;
+    if (t.isStringLiteral(args[0])) {
+        directory = args[0].value;
+    }
+
+    if (t.isBooleanLiteral(args[1])) {
+        includeSubdirectories = args[1].value;
+    }
+
+    if (t.isRegExpLiteral(args[2])) {
+        regularExpression = args[2].pattern;
+    }
+
+    const foldername = path.dirname(filename);
+    const folderDir = path.resolve(path.dirname(filename), directory);
+
+    const relativeFilepaths = glob
+        .sync(includeSubdirectories ? '**' : '*', {
+            dot: false,
+            nodir: true,
+            cwd: folderDir,
+        })
+        .filter(function (p) {
+            return new RegExp(regularExpression).test(p);
+        });
+
+    const absoulteFilepaths = relativeFilepaths
+        .map(function (p) {
+            return path.resolve(folderDir, p);
+        })
+        .map(function (p) {
+            return path.relative(foldername, p);
+        });
+
+    return absoulteFilepaths;
+}
 
 module.exports = function (filename, source) {
     const ast = parser.parse(source, {
@@ -16,54 +71,33 @@ module.exports = function (filename, source) {
     traverse(ast, {
         enter(p) {
             if (p.isCallExpression()) {
+                if (t.isMemberExpression(p.node.callee)) {
+                    if (
+                        t.isIdentifier(p.node.callee.object) &&
+                        p.node.callee.object.name === FUNCTION_NAME &&
+                        t.isIdentifier(p.node.callee.property) &&
+                        p.node.callee.property.name === PROPERTY_IMPORT
+                    ) {
+                        const args = p.node.arguments;
+                        const absoulteFilepaths = getFilepaths(filename, args);
+
+                        const nodes = createImportStatement(absoulteFilepaths);
+
+                        p.parentPath.replaceWithMultiple(nodes);
+                    }
+                }
+            }
+
+            if (p.isCallExpression()) {
                 if (
                     t.isIdentifier(p.node.callee) &&
                     p.node.callee.name === FUNCTION_NAME
                 ) {
                     const args = p.node.arguments;
-                    let directory, includeSubdirectories, regularExpression;
-                    if (t.isStringLiteral(args[0])) {
-                        directory = args[0].value;
-                    }
 
-                    if (t.isBooleanLiteral(args[1])) {
-                        includeSubdirectories = args[1].value;
-                    }
+                    const absoulteFilepaths = getFilepaths(filename, args);
 
-                    if (t.isRegExpLiteral(args[2])) {
-                        regularExpression = args[2].pattern;
-                    }
-
-                    const baseDir = path.dirname(filename);
-                    const folderDir = path.resolve(
-                        path.dirname(filename),
-                        directory
-                    );
-
-                    const relativeFilepaths = glob
-                        .sync(includeSubdirectories ? '**' : '*', {
-                            dot: false,
-                            nodir: true,
-                            cwd: folderDir,
-                        })
-                        .filter(function (p) {
-                            return new RegExp(regularExpression).test(p);
-                        });
-
-                    const absoulteFilepaths = relativeFilepaths.map(function (
-                        p
-                    ) {
-                        return path.resolve(folderDir, p);
-                    });
-
-                    const nodes = absoulteFilepaths.map(function (p) {
-                        return t.importDeclaration([], t.stringLiteral(p));
-                        // return t.expressionStatement(
-                        //     t.callExpression(t.identifier('require'), [
-                        //         t.stringLiteral(p),
-                        //     ])
-                        // );
-                    });
+                    const nodes = createRequireStatement(absoulteFilepaths);
 
                     p.parentPath.replaceWithMultiple(nodes);
                 }
